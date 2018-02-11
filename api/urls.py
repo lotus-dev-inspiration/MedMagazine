d7beddf31108398794e227796a44a87f466e49d6
Spyrot <spyrot.com@gmail.com>
2018-02-11 18:05:59 +0200

    index on feat/create-first-api: 10ec5e1 cors added from angular branch

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from .views import UserViewSet
 urlpatterns = [
-    path('get-user/', UserViewSet.as_view({'get': 'list'})),
+    path('getUser/', ),
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from .serialazers import UserSerializer
+from rest_framework.views import APIView
 
-# Serializers define the API representation.
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
+class UsersList(APIView):
+    
