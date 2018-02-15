be246ed42f082950d289f93cfb75344f46d58817
Spyrot <spyrot.com@gmail.com>
2018-02-15 17:25:29 +0200

    -Create method done

--- api/serialazers.py
+++ api/serialazers.py
@@ -16,7 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        exclude = ('groups','last_login','date_joined','user_permissions',)
+        exclude = ('groups','last_login','date_joined','user_permissions','is_superuser','password',)
 
     def create(self, validated_data):
         profile_data = validated_data.pop('profile')
@@ -24,5 +24,16 @@ class UserSerializer(serializers.ModelSerializer):
         Profile.objects.create(user=user,**profile_data)
         return user
 
-
-
+    def update(self, instance, validated_data):
+        instance.profile.phone = validated_data.get('profile.phone', instance.profile.phone)
+        instance.profile.company = validated_data.get('profile.company', instance.profile.company)
+        instance.profile.phone = validated_data.get('profile.patronymic', instance.profile.patronymic)
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get('password', instance.password)
+        instance.is_staff = validated_data.get('is_staff', instance.is_staff)
+        instance.is_active = validated_data.get('is_staff', instance.is_active)
+        instance.save()
+        return instance
--- api/views.py
+++ api/views.py
@@ -1,7 +1,15 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, mixins
 from .serialazers import UserSerializer
 from django.contrib.auth.models import User
 
-class UserViewset(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+# class UserViewset(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+class UserViewset(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
+    queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
