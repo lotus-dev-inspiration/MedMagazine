a16263de0537dbc68dc33068ff9dd517153a3c29
Valentyn Kyianytsia <valentine1004@ukr.net>
2018-03-03 14:37:28 +0200

    add style into login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Login from './components/login/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Login />
       </div>
     );
   }
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
+import './Login.css';
+import SignUp from './signUp/SignUp';
+import SignIn from './signIn/SignIn';
 
 class Login extends Component {
     render(){
         return (
             <div>
-                Login page
+                <div className="login-wrapper">
+                   <div className="login-image-wrapper">
+                      Image
+                   </div>
+                   <div className="login-main-field-wrapper">
+                     <SignUp />
+                   </div>
+                </div>
             </div>
         )
     }
