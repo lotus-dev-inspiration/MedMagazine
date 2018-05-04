df32b374df5a4590ae4b971b805aea3ffb027eab
Vlad Shyrinov <vlad.shyrinov@gmail.com>
2018-05-04 21:47:39 +0300

    added files for heroku

--- MedMagazine/settings.py
+++ MedMagazine/settings.py
@@ -17,6 +17,9 @@ import datetime
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -27,7 +30,7 @@ SECRET_KEY = '&q5z71@a9vz8kx41k5bx6uh!)x07*t(dh&_+8_*3kxb$6pw*!1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['scientific-magazine.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
