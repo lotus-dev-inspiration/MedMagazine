aba9c758291ce1db5b8beb7241b1debf15719193
Spyrot <spyrot.com@gmail.com>
2018-05-13 19:35:40 +0300

    -Models changed

--- api/models.py
+++ api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class ArticleStatus(models.Model):
     name = models.CharField(max_length=150, unique=True, blank=False)
 
@@ -11,11 +10,28 @@ class ArticleStatus(models.Model):
     def __str__(self):
         return self.name
 
+class ArticleStage(models.Model):
+    name = models.CharField(max_length=150, unique=True, blank=False)
+    statuses_id = models.ManyToManyField(ArticleStatus,on_delete=models.NOT_PROVIDED)
+
+    class Meta:
+        verbose_name_plural = "Article Stages"
+
+    def __str__(self):
+        return self.name + '\n%s' % self.statuses_id
+
 class Language(models.Model):
     language = models.CharField(max_length=200, primary_key=True, unique = True)
 
 class ArticleTheme(models.Model):
     theme = models.CharField(max_length=200, primary_key=True, unique = True)
+
+class Collaborator(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    patronymic = models.CharField(max_length=100, default='Not Provided')
+    position = models.CharField(max_length=200, default='Not Provided')
+    company = models.CharField(max_length=200, default='Not Provided')
 
 class Article(models.Model):
     name = models.CharField(max_length=200)
@@ -23,14 +39,22 @@ class Article(models.Model):
     description = models.CharField(max_length=3000)
     content = models.FileField(upload_to='pdf/')
     language = models.ForeignKey(Language, on_delete=models.NOT_PROVIDED)
+    udc = models.CharField(max_length=3000)
+    key_words = models.CharField(max_length=3000)
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,on_delete=models.NOT_PROVIDED)
+    collaborators = models.ManyToManyField(Collaborator, on_delete=models.NOT_PROVIDED)
     reviewers = models.ManyToManyField(User,related_name='reviewers',blank=True)
     status = models.ForeignKey(ArticleStatus, on_delete=models.NOT_PROVIDED,default=1)
+    stage = models.ForeignKey(ArticleStage,on_delete=models.NOT_PROVIDED, default=1)
+    number = models.IntegerField(default = 1)
+    can_edit = models.BooleanField(default=True)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, parent_link=True)
     patronymic = models.CharField(max_length=100, default='Not Provided')
+    position = models.CharField(max_length=200, default='Not Provided')
+    grade = models.CharField(max_length=200, default='Not Provided', blank=True)
     company = models.CharField(max_length=200, default='Not Provided')
     phone = models.CharField(max_length=20, default='Not Provided')
     articles = models.ManyToManyField(Article, blank=True)
@@ -41,3 +65,8 @@ class Comment(models.Model):
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
+class Journal(models.Model):
+    articles = models.ManyToManyField(Article)
+    date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(default = 'Журнал %s' % date)
+    theme = models.ForeignKey(ArticleTheme,on_delete=models.NOT_PROVIDED)
--- api/views.py
+++ api/views.py
@@ -7,13 +7,10 @@ from .models import Article, Comment
 from rest_framework.decorators import api_view, detail_route, list_route
 from rest_framework.response import Response
 from rest_framework_jwt.utils import jwt_payload_handler
-from rest_framework.pagination import PageNumberPagination
 from .models import Profile
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.settings import api_settings
 
-class ArticlesPaginator(PageNumberPagination):
-    page_size = 10
 
 class UserViewset(mixins.CreateModelMixin,
                   mixins.RetrieveModelMixin,
@@ -23,7 +20,6 @@ class UserViewset(mixins.CreateModelMixin,
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
     http_method_names = ['get', 'post', 'head', 'options', 'patch']
-    pagination_class = ArticlesPaginator
     
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -46,37 +42,32 @@ class UserViewset(mixins.CreateModelMixin,
     @detail_route(methods=['get'])
     def articles(self, request, pk=None):
 
-        def get_queryset(queryset):
-            author = self.request.query_params.get('author', None)
-            theme = self.request.query_params.get('theme', None)
-            year = self.request.query_params.get('year', None)
-            month = self.request.query_params.get('month', None)
-            language = self.request.query_params.get('language', None)
-            if author is not None:
-                queryset = queryset.filter(author=author)
-            if theme is not None:
-                queryset = queryset.filter(theme=theme)
-            if year is not None:
-                queryset = queryset.filter(date__year = year)
-            if month is not None:
-                queryset = queryset.filter(date__month = month)
-            if language is not None:
-                queryset = queryset.filter(language = language)
-            return queryset
+        # def get_queryset(queryset):
+        #     author = self.request.query_params.get('author', None)
+        #     theme = self.request.query_params.get('theme', None)
+        #     year = self.request.query_params.get('year', None)
+        #     month = self.request.query_params.get('month', None)
+        #     language = self.request.query_params.get('language', None)
+        #     if author is not None:
+        #         queryset = queryset.filter(author=author)
+        #     if theme is not None:
+        #         queryset = queryset.filter(theme=theme)
+        #     if year is not None:
+        #         queryset = queryset.filter(date__year = year)
+        #     if month is not None:
+        #         queryset = queryset.filter(date__month = month)
+        #     if language is not None:
+        #         queryset = queryset.filter(language = language)
+        #     return queryset
 
         try:
             profile = Profile.objects.get(pk=pk)
             queryset = profile.articles.all()
-            queryset = get_queryset(queryset)
             content = list()
 
             for article in queryset:
                 content.append(ArticleSerializer(article).data)
-            page = self.paginate_queryset(queryset)
 
-            if page is not None:
-                serializer = ArticleSerializer(page, many=True, context={'request': request})
-                return self.get_paginated_response(serializer.data)
             serializer = ArticleSerializer(queryset, many=True, context={'request': request})
             return Response(serializer.data)
 
@@ -92,45 +83,23 @@ class ArticleViewset(mixins.CreateModelMixin,
     queryset =  Article.objects.all()
     serializer_class = ArticleSerializer
     http_method_names = ['get', 'post', 'head','options','patch']
-    pagination_class = ArticlesPaginator
-    filter_backends = (filters.OrderingFilter,)
-    ordering = ('date')
 
-    def get_queryset(self):
-        queryset = Article.objects.all()
-        author = self.request.query_params.get('author', None)
-        theme = self.request.query_params.get('theme', None)
-        year = self.request.query_params.get('year', None)
-        month = self.request.query_params.get('month', None)
-        language = self.request.query_params.get('language', None)
-        if author is not None:
-            queryset = queryset.filter(author=author)
-        if theme is not None:
-            queryset = queryset.filter(theme=theme)
-        if year is not None:
-            queryset = queryset.filter(date__year = year)
-        if month is not None:
-            queryset = queryset.filter(date__month = month)
-        if language is not None:
-            queryset = queryset.filter(language = language)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response({"success": True, "message": "article created"}, status=status.HTTP_201_CREATED, headers=headers)
 
-        return queryset
+    def perform_create(self, serializer):
+        serializer.save()
 
-        def create(self, request, *args, **kwargs):
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
-            return Response({"success": True, "message": "article created"}, status=status.HTTP_201_CREATED, headers=headers)
+    def get_success_headers(self, data):
+        try:
+            return {'Location': str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {"success": False, "message": "not created"}
 
-        def perform_create(self, serializer):
-            serializer.save()
-
-        def get_success_headers(self, data):
-            try:
-                return {'Location': str(data[api_settings.URL_FIELD_NAME])}
-            except (TypeError, KeyError):
-                return {"success": False, "message": "not created"}
 
 class CommentViewset(mixins.CreateModelMixin,
                    mixins.RetrieveModelMixin,
